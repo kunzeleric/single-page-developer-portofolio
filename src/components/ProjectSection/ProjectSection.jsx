13d0c7ea04693874770e19bd9080dd7f6af4be54
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-04-20 14:34:35 -0300

    Iniciada sessão de projetos e exclusão de algumas imagens

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -2,7 +2,18 @@ import './index.scss';
 
 const ProjectSection = () => {
   return (
-    <div>ProjectSection</div>
+    <section className="project">
+      <div className="project__header">
+        <h1></h1>
+        <a href="#">contact me</a>
+      </div>
+      <h1></h1>
+      <div className="project__container">
+        <div className="project__container-card">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
