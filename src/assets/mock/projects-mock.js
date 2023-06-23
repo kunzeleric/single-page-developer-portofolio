39bf559fa769cb564fc918021433fc551970e53a
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-06-23 15:09:50 -0300

    Fixed a projects tech description

--- src/assets/mock/projects-mock.js
+++ src/assets/mock/projects-mock.js
@@ -52,7 +52,7 @@ export const PROJECTS =
         },
         {
             "name": "To-Do List",
-            "technologies": ["React", "Styled Components"],
+            "technologies": ["React", "Styled Components", "TypeScript"],
             "imgPath": "./todolist.jpg",
             "githubLink": "https://github.com/kunzeleric/ignite-todo-list",
             "netlifyLink": "https://tasklist-todos-ignite.netlify.app/"
